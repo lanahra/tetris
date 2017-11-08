4ca0ca5140c829dc192246e32c10635f60b66ab6
Leonardo Nahra <leonardonahra@gmail.com>
2017-11-08 19:05:08 +0100

    Add keyboard input to move tetromino sideways

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Color
 import Element exposing (..)
 import Grid exposing (Grid)
 import Html exposing (..)
+import Keyboard exposing (KeyCode)
 import Random exposing (Generator)
 import Set
 import Task
@@ -126,6 +127,7 @@ type Msg
   | RollTetromino
   | NewTetromino Tetromino
   | InitTetromino (Tetromino, Tetromino)
+  | KeyDown KeyCode
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -146,13 +148,16 @@ update msg model =
     InitTetromino tetrominos ->
       (initTetromino tetrominos model, Cmd.none)
 
+    KeyDown code ->
+      (input code model, Cmd.none)
+
 
 step : Model -> (Model, Cmd Msg)
 step model =
   case model.gameState of
     Running ->
-      if tetrominoCanDrop model.grid model.activeSquares then
-        (dropTetromino model, Cmd.none)
+      if tetrominoCanMove Down model.grid model.activeSquares then
+        (moveTetromino Down model, Cmd.none)
       else
         setGameState model
 
@@ -160,8 +165,31 @@ step model =
       (model, Cmd.none)
 
 
-dropTetromino : Model -> Model
-dropTetromino model =
+type Direction
+  = Left
+  | Up
+  | Right
+  | Down
+
+
+moveSquare : Direction -> (Int, Int) -> (Int, Int)
+moveSquare direction (row, column) =
+  case direction of
+    Left ->
+      (row, column - 1)
+
+    Up ->
+      (row - 1, column)
+
+    Right ->
+      (row, column + 1)
+
+    Down ->
+      (row + 1, column)
+
+
+moveTetromino : Direction -> Model -> Model
+moveTetromino direction model =
   let
     remove squares grid =
       case squares of
@@ -181,43 +209,46 @@ dropTetromino model =
           uncurry Grid.insert square model.activeTetromino grid
             |> insert rest
 
-    dropSquares =
-      List.map (\(row, column) -> (row + 1, column)) model.activeSquares
+    moveSquares =
+      List.map (moveSquare direction) model.activeSquares
 
-    drop =
+    move =
       remove model.activeSquares model.grid
-        |> insert dropSquares
+        |> insert moveSquares
   in
     { model
-        | grid = drop
-        , activeSquares = dropSquares
+        | grid = move
+        , activeSquares = moveSquares
     }
 
 
-tetrominoCanDrop : Grid Tetromino -> List (Int, Int) -> Bool
-tetrominoCanDrop grid squares =
+tetrominoCanMove : Direction -> Grid Tetromino -> List (Int, Int) -> Bool
+tetrominoCanMove direction grid squares =
   let
-    canDrop drops =
-      case drops of
+    canMove moves =
+      case moves of
         [] ->
           True
 
         (row, column) :: rest ->
           let
-            squareCanDrop =
-              Grid.memberRow row grid
-              && (Grid.member row column >> not) grid
+            squareCanMove =
+              0 <= row
+              && row < gridRows
+              && 0 <= column
+              && column < gridColumns
+              && (not << Grid.member row column) grid
           in
-            if squareCanDrop then
-              canDrop rest
+            if squareCanMove then
+              canMove rest
             else
               False
   in
-    List.map (\(row, column) -> (row + 1, column)) squares
+    List.map (moveSquare direction) squares
       |> Set.fromList
       |> flip Set.diff (Set.fromList squares)
       |> Set.toList
-      |> canDrop
+      |> canMove
 
 
 setGameState : Model -> (Model, Cmd Msg)
@@ -253,6 +284,33 @@ initTetromino init model =
       }
         |> initSquares
         |> initGrid
+
+
+input : KeyCode -> Model -> Model
+input code model =
+  case model.gameState of
+    Running ->
+      case code of
+        32 -> -- space bar
+          model
+
+        39 -> -- right arrow
+          if tetrominoCanMove Right model.grid model.activeSquares then
+            moveTetromino Right model
+          else
+            model
+
+        37 -> -- left arrow
+          if tetrominoCanMove Left model.grid model.activeSquares then
+            moveTetromino Left model
+          else
+            model
+
+        _ ->
+          model
+
+    Over ->
+      model
 
 
 -- COMMANDS
@@ -304,6 +362,7 @@ subscriptions model =
   Sub.batch
     [ Window.resizes (\size -> WindowResize size)
     , Time.every Time.second Step
+    , Keyboard.downs KeyDown
     ]
 
 
@@ -335,15 +394,15 @@ makeGrid grid =
   let
     makeElement ((row, column), square) =
       makeSquare square
-        |> moveSquare row column
+        |> transformSquare row column
   in
     Grid.toIndexedList grid
       |> List.map makeElement
       |> layers
 
 
-moveSquare : Int -> Int -> Element -> Element
-moveSquare row column square =
+transformSquare : Int -> Int -> Element -> Element
+transformSquare row column square =
   let
     (width, height) =
       (blockSize * gridColumns, blockSize * gridRows)
